a50bb08f4efb80e5311120c7ef712d2ca82da163
Marc Toensing <marc.toensing@gmail.com>
2021-05-02 13:11:08 +0200

    Update build-run.sh

--- build-run.sh
+++ build-run.sh
@@ -1,8 +1,7 @@
 docker build . -t marctv/papyruscs-local 
-docker scan
 docker run \
   --rm \
   -v /Users/mtoe/Downloads/mcserver-bedrock/DETROIT:/tmp/world/:rw \
   -v /Users/mtoe/Downloads/export:/tmp/export/:rw \
-  -e parameters=" -q 50 --limitx -10,10" \
+  -e parameters=" --limitx -10,10" \
   -it marctv/papyruscs-local
